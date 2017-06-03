05f68143f05b8a4734001647159c27011e156fa3
Sajeev Ramasamy <thorion3006@gmail.com>
2017-06-03 20:58:08 +0000

    User sign in: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+                      registrations: 'users/registrations',
+                      sessions: 'users/sessions',
+                      passwords: 'users/passwords',
+                      confirmations: 'users/confirmations'
+                    }
   root 'welcome#index'
 
   # For LetterOpenerWeb
